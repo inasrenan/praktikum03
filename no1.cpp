b71af33ad35f529f54c0d34d93c53d6073fc7a43
inasrenan <inasrenan@github.com>
2017-11-08 00:39:10 +0700

    penyelesayan soal no.1

---
+++ no1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	int X, Y, Z;
+	cout<<"Masukan X\t\t =  "; cin>>X;
+	cout<<"Masukan Y\t\t =  "; cin>>Y;
+	if(X<Y){
+		Z=Y.X=;
+	}
+	else{
+		Z=X.Y;
+	}
+	cout<<"Hasil Pengurangan \t = "<<Z;
+}
