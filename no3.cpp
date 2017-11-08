e765645b1a3b7561ab75bc9735c175725b0955de
inasrenan <inasrenan@github.com>
2017-11-08 13:03:51 +0700

    melakukan perbaikan

--- no3.cpp
+++ no3.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-	double jk,lr,jl,jatot;
+	double jk,lb,jl,jtot;
 	long gtot,gaber;
 	
 	cout<<"PROGRAM HITUNG GAJI"<<endl;
@@ -13,29 +13,29 @@ int main(){
 	if (jk>40){
 		cout<<"Gaji Pokok\t\t = Rp 2000.000"<<endl;
 		cout<<"Jam Kerja Standar\t = 40 jm"<<endl;
-		lb=jk.40;
+		lb=jk-40;
 		cout<<"Lembur\t\t = "<<lb<<" jam"<<endl; 
 		jl=lb*1.5;
-		jatot=40+jl;
+		jtot=40+jl;
 		cout<<"Total Jam Kerja\t\t = "<<jtot<<" jam"<<endl;
 		gtot=2000000+(jl*100000);
 		cout<<"Total Gaji \t\t = "<<"Rp "<<gtot<<endl;
 		cout<<"Potongan \t\t = 15%"<<endl;
 		gaber=gtot-(0.15*gtot);
-		cout<<"Gaji Bersih\t\t = "<<"Rp "<<Gaber;
+		cout<<"Gaji Bersih\t\t = "<<"Rp "<<gaber;
 	}
-	elese if (jk<=40){
+	else if (jk<=40){
 		cout<<"Gaji Pokok\t\t = Rp 2000.000"<<endl;
 		cout<<"Jam Kerja Standar\t = 40 jam"<<endl;
-		lb=0
-		cout<<"lembur\t\t\t = "<<lb<<" jam"<<endl;
+		lb=0;
+		cout<<"Lembur\t\t\t = "<<lb<<" jam"<<endl;
 		jl=lb*1.5;
 		jtot=40+jl;
-		cout<<"Total Jam Kerja \t\t = "<<Jtot<<" jam"<<endl;
-		gatot=2000000+(jl*100000);
+		cout<<"Total Jam Kerja \t\t = "<<jtot<<" jam"<<endl;
+		gtot=2000000+(jl*100000);
 		cout<<"Total Gaji \t\t = "<<"Rp "<<gtot<<endl;
 		cout<<"Potongan \t\t = 15%"<<endl;
-		gaber=gtot.(0.15*gtot);
+		gaber=gtot-(0.15*gtot);
 		cout<<"Gaji Bersih\t\t = "<<"Rp "<<gaber;
 	}
 	return 0;
